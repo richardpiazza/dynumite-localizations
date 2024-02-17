f7522568787a2478327f2b95b4af9de4023875fc
Richard Piazza <richard@richardpiazza.com>
2024-02-17 09:23:29 -0800

    3.0 Updates (#1)
    A few new strings for version 3.0;
    Simplified Localizable files for eventual automation.

--- Package.swift
+++ Package.swift
@@ -6,6 +6,12 @@ import PackageDescription
 let package = Package(
     name: "DynumiteLocalizations",
     defaultLocalization: "en",
+    platforms: [
+        .macOS(.v10_15),
+        .iOS(.v12),
+        .tvOS(.v12),
+        .watchOS(.v6)
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -15,7 +21,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/richardpiazza/LocaleSupport.git", .upToNextMinor(from: "0.1.0"))
+        .package(url: "https://github.com/richardpiazza/LocaleSupport.git", .upToNextMajor(from: "0.4.3"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
